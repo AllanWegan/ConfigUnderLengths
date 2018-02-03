9810ac16d7db1772c4cf019bbec4edf745de4b55
Allan Wegan <allanwegan@allanwegan.de>
2018-02-03 14:06:09 +0100

    Version 0.0.3 of Longer Underground Belts, MIT-licensed, by goobie, https://mods.factorio.com/mod/longer-undergrounds

---
+++ src/data.lua
@@ -0,0 +1,4 @@
+data.raw["pipe-to-ground"]["pipe-to-ground"].fluid_box.pipe_connections[2].max_underground_distance = 30
+data.raw["underground-belt"]["underground-belt"].max_distance = 10
+data.raw["underground-belt"]["fast-underground-belt"].max_distance = 20
+data.raw["underground-belt"]["express-underground-belt"].max_distance = 30
